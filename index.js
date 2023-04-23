f4af52d23177ab991831e1726d36449f08735631
tanisha-singhal <tanusinghal1000@gmail.com>
2023-04-23 15:45:04 +0530

    commit

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ io.on('connection',(socket)=>{
         const {error,user}=addUser({id:socket.id,name,room});
         console.log(user);
         if(error) return callback(error);
-
+        socket.join(user.room);
         socket.emit('message',{user:'admin',text:`${user.name},welcome to the room ${user.room}`});
         socket.broadcast.to(user.room).emit('message',{user:'admin',text:`${user.name},has joined! `});
 
-        socket.join(user.room);
+        
         io.to(user.room).emit('roomData',{room:user.room,users:getUsersInRoom(user.room)})
-        console.log(getUsersInRoom(user.room));
+        //console.log(getUsersInRoom(user.room));
         callback();
         
     })
@@ -36,7 +36,7 @@ io.on('connection',(socket)=>{
     socket.on('sendMessage',(message,callback)=>{
         const user=getUser(socket.id);
         io.to(user.room).emit('message',{user:user.name,text:message});
-        io.to(user.room).emit('roomData',{room:user.room,users:getUsersInRoom(user.room)});
+        //io.to(user.room).emit('roomData',{room:user.room,users:getUsersInRoom(user.room)});
         callback();
     });
     socket.on('disconnect',()=>{
